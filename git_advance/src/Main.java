bc948074c898b3ecd7e076b76924791d91c1646d
Akhror Mamasodirov <ni3@qad.com>
2024-07-10 16:42:44 +0500

    fixed MR comments

--- git_advance/src/Main.java
+++ git_advance/src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         sum(3,4);
-        minussss(10,4);
+        minus(10,4);
     }
 
     public static void sum(int a, int b) {
         System.out.println(a + b);
     }
 
-    public static void minussss(int a, int b) {
-        System.out.println(a + b);
+    public static void minus(int a, int b) {
+        System.out.println(a - b);
     }
 }
